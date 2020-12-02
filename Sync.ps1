f7454cb2a02675a57e8a4ac336bc5b41328d6bb0
Tom Cumbow <tcumbow@icloud.com>
2020-12-02 17:53:32 -0500

    folderized

---
+++ Sync.ps1
@@ -0,0 +1,3 @@
+unison .\ChatWindowManager A:\ChatWindowManager -auto -batch
+pause
+unison .\ChatWindowManager "\\samantha-gaming\Users\saman\OneDrive\Documents\Elder Scrolls Online\live\AddOns\ChatWindowManager" -auto -batch
